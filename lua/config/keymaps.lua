4be7bb95f112ecae6f8b777d6e54fde5ef7126e2
edieguez <dieguezemmanuel@gmail.com>
2026-06-25 12:11:46 -0600

    Add quit and save keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 -- Add jk as alternative to Esc
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
+
+-- Save all buffers and quit
+vim.keymap.set("n", "<leader>qW", "<cmd>wqall<cr>", { desc = "Save all and quit" })
+
+-- Save all buffers (without quitting)
+vim.keymap.set("n", "<leader>qw", "<cmd>wall<cr>", { desc = "Save all buffers" })
+
+-- Quit all, discarding unsaved changes (force)
+vim.keymap.set("n", "<leader>qZ", "<cmd>qall!<cr>", { desc = "Quit all (force, no save)" })
 
 vim.keymap.set({ "n", "x" }, "y", function()
   return (vim.v.register == '"' and '"+' or '"' .. vim.v.register) .. "y"
