6549a448289e8284c97ff7f0304ad299146c27dc
edieguez <dieguezemmanuel@gmail.com>
2026-06-25 08:38:10 -0600

    Customize snacks
    - Move explorer to the right
    - Move notifier to the right bottom

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      layouts = {
+        sidebar = {
+          layout = {
+            position = "right",
+          },
+        },
+      },
+    },
+    notifier = {
+      top_down = false,
+    },
+  },
+}
