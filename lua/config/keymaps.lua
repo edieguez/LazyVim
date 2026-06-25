bb67743d11020cdaa17ea6025b3fba4e24bc88b0
edieguez <dieguezemmanuel@gmail.com>
2026-06-25 09:03:07 -0600

    Add ergonomics for clipboard and Esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Add jk as alternative to Esc
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
+
+vim.keymap.set({ "n", "x" }, "y", function()
+  return (vim.v.register == '"' and '"+' or '"' .. vim.v.register) .. "y"
+end, { expr = true, desc = "Yank to system clipboard" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.opt.clipboard = "" -- Disable OS clipboard sync
+vim.opt.clipboard = "" -- Disable OS clipboard sync
 vim.opt.spell = true -- Enable spellchecking
 vim.opt.spelllang = { "en", "es" }
 vim.g.autoformat = false -- Disable autoformat on save
